92bc1214ca64c102ec8136db2e6ff9bda58b3eac
Kevin <kevinzhang3303@gmail.com>
2024-10-04 17:38:11 -0400

    Updated deployed URL

--- src/App.js
+++ src/App.js
@@ -68,7 +68,7 @@ class App extends Component{
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
 
-    fetch('http://localhost:3000/clarifai', {
+    fetch('https://face-recognition-application-backend.onrender.com/clarifai', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -78,7 +78,7 @@ class App extends Component{
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://face-recognition-application-backend.onrender.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
